f455cf87118c670f856ecd2bc6aa6b8efca7e9ab
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-03-04 14:39:53 +1000

    Create tutorial1.py

---
+++ tutorial1.py
@@ -0,0 +1,36 @@
+# Exercise 1
+print(88*23948)
+x = 'Andy'
+y = 'Brown'
+print(x+' '+y)
+
+# Exercise 2
+x = ['Jeff',
+'Nadia','Li']
+x.insert(0,'Erika')
+print(x[0])
+
+# Exercise 3
+x = [5,8,4,1]
+y = sum(x)
+print(y)
+
+# Exercise 4
+x = [4,6,7,8,4,2]
+print(min(x))
+print(max(x))
+print(x[:4])
+
+# Exercise 5
+x = [
+	{'Name':'Brad','Score':38},
+	{'Name':'Lisa','Score':58},
+	{'Name':'Achmad','Score':78},
+]
+x[1]['Score'] = 89
+print(x[0]['Score'])
+
+# Exercise 6
+x = 'I like turtles'
+x = x.split(' ')
+print(x[-1])
