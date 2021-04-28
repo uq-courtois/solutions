b295ba98553588da4b51287e2411597daff7fc84
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-04-28 11:47:42 +1000

    Update gradedexercises.py

--- gradedexercises.py
+++ gradedexercises.py
@@ -90,3 +90,42 @@ for item in data:
 data = pd.DataFrame(datatowrite)
 data.to_csv('artistsong_filtered.csv',sep=';',index=False)
 
+# Exercise 4 - Module 3
+
+from urllib.request import Request, urlopen
+import ssl
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = 'https://www.abc.net.au/news/justin'
+
+headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Macinstosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+}
+req = Request(url, headers=headers)
+context = ssl._create_unverified_context()
+
+uClient = urlopen(req, context=context)
+html = uClient.read()
+uClient.close()
+data = []
+
+soup = BeautifulSoup(html, 'html.parser')
+
+divofinterest = soup.find('div', class_='_3OXQ1 _26IxR _3bGVu')
+
+for item in divofinterest.find_all('div', class_='_3bhpO'):
+    url = item.find('a')['href']
+    url = 'https://www.abc.net.au/news/justin' + url
+    title = (item.find('h3').getText())
+    description = (item.find('div',class_='_3P1Sq _1deB8 _1hGzz _1-RZJ _1yL-m').getText())
+
+    data.append({
+		'title':title,
+		'url':url,
+		'description':description
+	})
+
+datatowrite = pd.DataFrame(data)
+datatowrite.to_csv('abcnews.csv',sep=';',index=False)
