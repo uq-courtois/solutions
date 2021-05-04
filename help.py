ec4a78b5cb982c305082a38edbc29d912bf703d3
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-05-04 11:56:22 +1000

    Create help.py

---
+++ help.py
@@ -0,0 +1,34 @@
+### URLS
+
+#http://www.digitalanalytics.id.au/static/files/billboard1.csv
+#http://www.digitalanalytics.id.au/static/files/billboard2.csv
+
+#http://www.digitalanalytics.id.au/static/files/billboard_additional.csv
+  
+### Exercise 7
+
+def decadef(x):
+	if x >= 1990 and x < 2000:
+		decade = '1990s'
+	if x >= 2000 and x < 2010:
+		decade = '2000s'
+	if x >= 2010 and x < 2020:
+		decade = '2010s'
+	if x >= 2020:
+		decade = '2020s'
+	
+	return decade
+
+df['decade'] = df['year'].apply(lambda x: decadef(x))
+
+def popularityf(x):
+	if x < 33:
+		popularity = 'low'
+	if x >= 33 and x < 66:
+		popularity = 'medium'
+	if x >= 66:
+		popularity = 'high'
+
+	return popularity
+
+df['popularitycat'] = df['popularity'].apply(lambda x: popularityf(x))
