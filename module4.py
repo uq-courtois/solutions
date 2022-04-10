eaf15ed075d3be316267bb079c867078e51483df
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2022-04-10 12:40:16 +1000

    Create module4.py

---
+++ module4.py
@@ -0,0 +1,25 @@
+### Exercise 1
+
+import json
+
+data = {
+   "items": [
+		 {
+			 'shape':'rectangle',
+			 'length':4,
+			 'width':5
+		 },
+		 {
+			 'shape':'square',
+			 'length':4,
+			 'width':4
+		 },
+		 {
+			 'shape':'cube',
+			 'length':4,
+			 'width':4,
+			 'heigth':4
+		 },
+   ]
+}
+print(json.dumps(data, sort_keys=True, indent=3)) 
