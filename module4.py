f236b94ddbc02c53a7c565176111407619ee4fbc
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2022-04-11 16:30:46 +1000

    Update module4.py

--- module4.py
+++ module4.py
@@ -1,6 +1,7 @@
 ### Exercise 1
 
 import json
+# Needed for exercises 1-3 -> copy/paste until row of hashes
 
 data = {
    "items": [
@@ -23,3 +24,25 @@ data = {
    ]
 }
 print(json.dumps(data, sort_keys=True, indent=3)) 
+
+###############################################################
+### ADDITIONAL
+
+import requests
+import json
+
+headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer BQAutymB1gVCft1ra_LiMAYq-rXsTVsGfFBs0QbneWcGSCEru_ajkM6NbOvuDY1ocQg3N8cpmu1mguCuT5pmebkAv0-gnl00H5UTfohbbReSSRM0uPHTmyhjXjGBTBmrsi8rXabITrGTfdP0pWxJ8C-p1cWp8eVaHQuGmHz9jjl_St8TtjlQT2_7a6Hvz-rQqst8UHze8UhRN8ryjuIN42Z5znqRuVmzxJhDn0UiJxl5ZydiMZUONJwrCbZqP9rXCPA7I6PGopG5V8ON5PVUtDI',
+}
+
+params = {
+    'time_range': 'medium_term',
+}
+
+response = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers, params=params)
+data = json.loads(response.text) 
+
+for i in data['items']:
+	print(i['name'])
