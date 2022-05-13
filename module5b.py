5f36283bd7d3be0bc01992f3920643ddf1c7fd00
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2022-05-13 19:55:30 +1000

    Update module5b.py

--- module5b.py
+++ module5b.py
@@ -31,7 +31,7 @@ print(catsum)
 
 ### Exercise 3
 
-# Fork code from: https://replit.com/@CedricCourtois/RegularKnowingAtoms#main.py
+# Fork code from: https://replit.com/@ZoeMacLean/Week-10-Exercises
 # Describe the variable popularitycat
 # Get the median of that variable
 # Visualize the distribution of the variable
@@ -106,3 +106,107 @@ plt.tight_layout()
 plt.savefig('histo_acousticness.pdf')
 plt.clf() 
 
+### Exercise 5
+# Fork code from: https://replit.com/@ZoeMacLean/Week-10-Exercises
+# Describe and visualize the relationship between the variables popularitycat and decade
+
+import pandas as pd
+import researchpy as rp
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('http://www.digitalanalytics.id.au/static/files/billboard_merged.csv',sep=',') 
+
+def categorization(popularity):
+	if popularity >= 50:
+		popularitycat = 'High popularity'
+	else:
+		popularitycat = 'Low popularity'
+	return popularitycat
+
+df['popularitycat'] = df['popularity'].apply(lambda x: categorization(x))
+
+def categorization(year):
+	if year >= 1990 and year < 2000:
+		decade = '1990s'
+	if year >= 2000 and year < 2010:
+		decade = '2000s'
+	if year >= 2010:
+		decade = '2010s'
+	return decade
+	
+df['decade'] = df['year'].apply(lambda x: categorization(x))
+
+# CROSSTABULATION
+table, results = rp.crosstab(df['popularitycat'],df['decade'], prop= 'col', test= 'chi-square')
+ 
+print(table) # Prints crosstab
+print()
+print(results) # Prints statistics tab
+ 
+# STACKED BAR CHART
+ct = pd.crosstab(df['decade'],df['popularitycat'],normalize='index') # Create PD crosstab as basis for plot
+ct.plot.bar(stacked=True) # Generate plot
+plt.legend(loc='lower left', bbox_to_anchor= (0.0, 1.01),frameon=False) # Tidy up legend (other)
+plt.xticks(rotation="horizontal") # Horizontal x-axis labels
+plt.xlabel('Decade') # Adding label on x-axis
+plt.ylabel('Popularity') # Adding label on y-axis
+plt.tight_layout() # Forces tidy plot lay-out
+plt.savefig('stackedbar.pdf') # Saving figure
+ 
+# Clear figure
+plt.clf() 
+
+### Exercise 6
+
+# Read the data file http://www.digitalanalytics.id.au/static/files/billboard_merged.csv
+# Describe and visualize the relationship between the variables popularity and danceability
+
+import pandas as pd
+import researchpy as rp
+import matplotlib.pyplot as plt
+ 
+df = pd.read_csv('http://www.digitalanalytics.id.au/static/files/billboard_merged.csv', delimiter = ',') 
+ 
+### CORRELATION
+print(rp.correlation.corr_pair(df[['popularity', 'danceability']]))
+ 
+### SCATTERPLOT
+plt.scatter(df['popularity'],df['danceability']) # Build plot
+plt.xlabel('Popularity') # x-axis label
+plt.ylabel('Danceability') # y-axis label
+plt.tight_layout() # Forces tidy plot lay-out
+plt.savefig('scatterplot.pdf') # Save figure
+plt.clf() # Clear figure
+
+### Exercise 7
+
+# Read the data file http://www.digitalanalytics.id.au/static/files/billboard_merged.csv
+# Aggregate the dataset into a new dataframe named grouped so you get the standard deviation of danceability per year (year being the grouping variable)
+# Add another variable: acousticness
+
+import pandas as pd
+ 
+df = pd.read_csv('http://www.digitalanalytics.id.au/static/files/billboard_merged.csv', delimiter = ',') 
+x = grouped.groupby(['year'], as_index=False)[['danceability','acousticness']].std()
+print(x)
+
+### Exercise 8
+
+# Plot the relationship between the standard deviations of danceability and acousticness and year measures in the previously constructed dataframe named grouped (i.e., construct a time series graph)
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('http://www.digitalanalytics.id.au/static/files/billboard_merged.csv', delimiter = ',') 
+
+x = df.groupby(['year'], as_index=False)[['danceability','acousticness']].std()
+
+print(x)
+
+# Time series
+plt.plot(x['year'],x['danceability'],label="Danceability")
+plt.plot(x['year'],x['acousticness'],label="Accousticness")
+plt.legend()
+plt.tight_layout()
+plt.savefig('timeseries.pdf')
+plt.clf()
